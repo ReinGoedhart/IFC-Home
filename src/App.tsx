7bea47778b20d1a6fc47053e9e8e120b239e7de3
ReinGoedhart <RWgoedhart@gmail.com>
2024-06-04 21:20:01 +0200

    adjust for build

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import ThreeDWorld from "./Components/HelloWorldComponent/HelloWorldComponent";
 import LoadIfcComponent from "./Components/LoadIfcComponent/LoadIfcComponent";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <LoadIfcComponent></LoadIfcComponent>
